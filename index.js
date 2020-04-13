5f7b8c6f7e88ae6f2ddad5c9ac3bd768c1f7f1ef
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 05:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -111,23 +111,14 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
-}
-
-document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
   } else if (e.which === 39) {
      moveDodgerRight()
   }
-})
+}
+
+document.addEventListener('keydown', moveDodger(e))
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
