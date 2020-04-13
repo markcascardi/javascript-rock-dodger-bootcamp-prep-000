7a833712305b3dbc001257eac7bb58993dc45a11
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 05:18:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  if (e.which === 37) {
+  if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
-  } else if (e.which === 39) {
+  } else if (e.which === RIGHT_ARROW) {
      moveDodgerRight()
   }
 }
